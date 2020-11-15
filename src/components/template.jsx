d5ac80e71f0217612a6b78ed78331ca29439bf9b
BalrokHS <kostas.lianos.96@gmail.com>
2020-11-16 00:33:55 +0200

    components folder added

---
+++ src/components/template.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+class  Templates extends Component {
+    // state = {  }
+    render() { 
+        return <h1>This is a new test</h1>;
+    }
+}
+ 
+export default Templates ;
